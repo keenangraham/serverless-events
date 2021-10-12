78f8cf6d69eb5d0763c59820bb2a295ae73bf5a0
Keenan Graham <keenangraham@stanford.edu>
2021-10-12 14:14:53 -0700

    Update

--- serverless_events/serverless_events_stack.py
+++ serverless_events/serverless_events_stack.py
@@ -1,10 +1,8 @@
+from aws_cdk import aws_lambda
+from aws_cdk import aws_apigateway
 from aws_cdk import core as cdk
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_solutions_constructs import aws_apigateway_lambda
 
 
 class ServerlessEventsStack(cdk.Stack):
@@ -12,4 +10,17 @@ class ServerlessEventsStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        aws_apigateway_lambda.ApiGatewayToLambda(
+            self,
+            'ApiGatewayToLambda',
+            lambda_function_props=aws_lambda.FunctionProps(
+                runtime=aws_lambda.Runtime.PYTHON_3_8,
+                code=aws_lambda.Code.asset('serverless_events/lambda/'),
+                handler='test.handler',
+            ),
+            api_gateway_props=aws_apigateway.RestApiProps(
+                default_method_options=aws_apigateway.MethodOptions(
+                    authorization_type=aws_apigateway.AuthorizationType.NONE,
+                )
+            )
+        )
