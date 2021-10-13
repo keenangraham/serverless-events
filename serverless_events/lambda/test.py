d8aa18fc5672343cec8703f2d22123285b2b7447
Keenan Graham <keenangraham@stanford.edu>
2021-10-12 17:50:43 -0700

    Update function

--- serverless_events/lambda/test.py
+++ serverless_events/lambda/test.py
@@ -1,12 +1,26 @@
 import json
 
 
+def make_query_string_from_grouped_params(grouped_params):
+    key_values = (
+        f'{param_key}={param_value}'
+        for param_key, param_values in grouped_params.items()
+        for param_value in param_values
+    )
+    return '&'.join(key_values)
+
+
+def make_query_string_from_event(event):
+    grouped_params =  event.get('multiValueQueryStringParameters', {})
+    return make_query_string_from_grouped_params(grouped_params)
+
+
 def handler(event, context):
-    print(f'request: {json.dumps(event)}')
+    query_string = make_query_string_from_event(event)
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Test'
+        'body': f'?{query_string}'
     }
